a11466fcc7902412da2fe01fc636de2e6f0cdc90
Lucas Lemos <balahallslemos@gmail.com>
2023-09-28 10:46:27 -0300

    mascara cpf feita

--- Aula05/appcrood/src/app/pagina/addusuario/addusuario.page.ts
+++ Aula05/appcrood/src/app/pagina/addusuario/addusuario.page.ts
@@ -24,10 +24,10 @@ export class AddusuarioPage implements OnInit {
     nivel: '',
   }
 
-  
 
-  constructor(private modalCtrl: ModalController, private service: UsuarioService,
-    private toastCtrl: ToastController) { }
+  onlyNumbers: any = new RegExp('^[0-9]+$');
+
+  constructor(private modalCtrl: ModalController, private service: UsuarioService, private toastCtrl: ToastController) { }
 
   ngOnInit() {
 
@@ -41,10 +41,12 @@ export class AddusuarioPage implements OnInit {
 
   }
 
+
+
   async enviando(form: NgForm) {
     //console.log(form.value); // pegar a informação e enviar no console
     const usuario = form.value;
-    const cpf = form.value;
+    let cpfExist;
 
     if (!usuario.nome ||
       !usuario.email ||
@@ -55,7 +57,7 @@ export class AddusuarioPage implements OnInit {
 
     }
 
-   
+
 
 
     else if (this.atualizar) {
@@ -63,28 +65,60 @@ export class AddusuarioPage implements OnInit {
         // fechar o modal
         this.modalCtrl.dismiss(response);
       })
-    }
-    else {
+    }else {
+      if (this.onlyNumbers.test(usuario.cpf)) {
+        /*segunda condiçao sera responsavel por verificar se ja existe
+         um cpf igual ao que foi inserido*/
+        usuario.cpf = this.getCpfFormated(usuario.cpf);
+        cpfExist = await firstValueFrom(this.service.getCPF(usuario.cpf));
+        if (cpfExist) {
+          // caso de false, a variavel 'msg' receberá um valor e campo será limpo
+
+          this.dados.cpf = '';
+          this.mensagem('este cpf ja existe');
+        }
+      } else {
+        // caso de false, a variavel 'msg' receberá um valor e campo será limpo
+        this.dados.cpf = '';
+        this.mensagem('Somente numeros no campo CPF');
+        cpfExist = true;
+      }
       const emailExist = await firstValueFrom(this.service.getEmail(usuario.email));
-      const getCPF = await firstValueFrom(this.service.getCPF(usuario.cpf));
+      //const getCPF = await firstValueFrom(this.service.getCPF(usuario.cpf));
       if (emailExist) {
         this.mensagem('Este email já existe.');
       }
 
-      else if (getCPF) {
-        this.mensagem('Este CPF já existe.');
-      }
-      
+
       else {
         this.service.create(usuario).subscribe(response => {
           // fechar o modal
           this.modalCtrl.dismiss(response);
         })
+        /* else if (getCPF) {
+           this.mensagem('Este CPF já existe.');*/
       }
-    }
 
+    }
   }
 
+
+
+
+  getCpfFormated(cpf: string): string {
+    //array com os cada parte do cpf
+    let cpfGroup: Array<string> = [
+      `${cpf[0]}${cpf[1]}${cpf[2]}`,
+      `${cpf[3]}${cpf[4]}${cpf[5]}`,
+      `${cpf[6]}${cpf[7]}${cpf[8]}`,
+      `${cpf[9]}${cpf[10]}`
+    ];
+    //formando o cpf formatado
+    let formattedCpf: string = `${cpfGroup[0]}.${cpfGroup[1]}.${cpfGroup[2]}-${cpfGroup[3]}`;
+    return formattedCpf;
+
+
+  }
 
   fecharModal() {
     this.modalCtrl.dismiss();
@@ -98,7 +132,12 @@ export class AddusuarioPage implements OnInit {
       toast.present();
     })
   }
-
-
 }
+
+
+
+
+
+
+
 
